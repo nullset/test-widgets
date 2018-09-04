928db210aa95392f04b7eade11dceafa6256d088
Nathan Wright <nathan@brandalism.com>
2018-09-04 16:30:25 -0500

    starting from aha-app version

--- tooltip.js/custom.js
+++ tooltip.js/custom.js
@@ -1,5 +1,5 @@
 (function($) {
-  const namespace = 'ahaTooltip';
+  const namespace = 'AhaTooltip';
 
   // ---------- DEFAULT SETTINGS ----------
   const popperDefaults = {
@@ -16,7 +16,7 @@
       },
       hide: {
         enabled: true,
-      }
+      },
     },
     preventOverflow: {
       boundariesElement: 'scrollView',
@@ -26,6 +26,7 @@
   const ahaTooltipDefaults = {
     popper: popperDefaults,
     html: false,
+    cache: false,
     trigger: 'hover focus',
     type: 'tooltip',
     delay: {
@@ -43,39 +44,42 @@
     this.enabled = true;
     this.isVisible = false;
     this.createTooltip(triggerElem, opts);
-  }
+  };
 
   AhaTooltip.prototype.openTooltip = function() {
     const data = AhaTooltip.prototype.getData(this.triggerElem);
     const self = this;
 
     // Close any other type of tooltip that is open for this triggering element.
-    Object.keys(data).forEach(function(type) {
+    Object.keys(data).forEach((type) => {
       if (type !== self.opts.type && type !== 'popper') {
         data[type].tooltip.closeTooltip(0);
       }
     });
+    this.getContents();
 
     this.appendTooltip();
-  }
+  };
 
   AhaTooltip.prototype.appendTooltip = function() {
     if (this.enabled) {
       clearTimeout(this.timeout);
       this.isVisible = true;
       this.timeout = setTimeout(() => {
-        this.triggerElem.setAttribute('x-tooltip', '');
-        this.repositionTooltip(this.triggerElem, this.type);
-        const container = this.opts.container ? document.querySelector(this.opts.container) : this.triggerElem;
-        container.appendChild(this.popper.popper);
-        requestAnimationFrame(() => {
-          if (!this.popper.popper.hasAttribute('x-in')) {
-            this.popper.popper.setAttribute('x-in', '');
-          }
-        });
+        if (this.isVisible) {
+          this.triggerElem.setAttribute('x-tooltip', '');
+          this.repositionTooltip(this.triggerElem, this.type);
+          const container = this.opts.container ? document.querySelector(this.opts.container) : this.triggerElem;
+          container.appendChild(this.popper.popper);
+          requestAnimationFrame(() => {
+            if (!this.popper.popper.hasAttribute('x-in')) {
+              this.popper.popper.setAttribute('x-in', '');
+            }
+          });
+        }
       }, this.opts.delay.show || 0);
     }
-  }
+  };
 
   AhaTooltip.prototype.getTemplate = function() {
     const $template = this.opts.template
@@ -88,7 +92,7 @@
       </div>
     </div>`);
     return $template[0];
-  }
+  };
 
   AhaTooltip.prototype.createTooltip = function() {
     const template = this.getTemplate();
@@ -99,39 +103,13 @@
 
     this.popper = new Popper(this.triggerElem, template, this.opts.popper);
 
-    const data = AhaTooltip.prototype.getData(this.triggerElem);
+    const data = this.data = AhaTooltip.prototype.getData(this.triggerElem);
     data[this.type] = {
       tooltip: this,
       popper: this.popper,
-    }
-    $(this.triggerElem).data(namespace, data);
+    };
 
-    const self = this;
-    const resolveURL = new Promise(function(resolve, reject) {
-      if (self.opts.url) {
-        self.popper.popper.removeAttribute('x-loading-error');
-        self.popper.popper.setAttribute('x-loading', '');
-        self.opts.content = '<i class="fa fa-spinner fa-spin"></i>';
-        self.updateTooltipContent(self.triggerElem, self.type);
-        $.ajax({
-          url: self.opts.url,
-          success: (data) => {
-            self.opts.content = data;
-          },
-          error: (data) => {
-            self.popper.popper.setAttribute('x-loading-error', '');
-          },
-          complete: (data) => {
-            self.popper.popper.removeAttribute('x-loading');
-            resolve();
-          }
-        });
-      } else {
-        resolve();
-      }
-    }).then(() => {
-      self.updateTooltipContent(self.triggerElem, self.type);
-    });
+    this.setData(data);
 
     // Watch for changes to title, data-title, data-tooltip, data-content and update the tooltip contents accordingly.
     // This enables us to change the title/data-title/data-tooltip/data-content of the tooltip triggering element
@@ -141,40 +119,113 @@
       attributes: true,
       attributeFilter: ['title', 'data-tooltip', 'data-title', 'data-content'],
     });
-  }
+
+    this.triggerElemMutationObserver = new MutationObserver(this.triggerElemMutationCallback.bind(this, this.triggerElem));
+    this.triggerElemMutationObserver.observe(this.triggerElem.parentNode, {
+      childList: true,
+    });
+  };
+
+  AhaTooltip.prototype.setPreventEventExcept = function(eventName) {
+    const data = this.getData(this.triggerElem);
+    Object.defineProperty(data, 'preventEventExcept', {
+      value: eventName,
+      enumerable: false,
+      configurable: true,
+    });
+  };
+
+  AhaTooltip.prototype.clearPreventEventExcept = function() {
+    const data = this.getData(this.triggerElem);
+    delete data.preventEventExcept;
+  };
+
+  AhaTooltip.prototype.getContents = function() {
+    new Promise((resolve, reject) => {
+      if (this.opts.url && (!this.opts.content || this.opts.cache === false)) {
+        this.popper.popper.removeAttribute('x-loading-error');
+        this.popper.popper.setAttribute('x-loading', '');
+        this.opts.content = '<i class="fa fa-refresh fa-spin fa-lg"></i>';
+        this.updateTooltipContent(this.triggerElem, this.type);
+        $.ajax({
+          url: this.opts.url,
+          success: (data) => {
+            this.opts.content = data;
+          },
+          error: (data) => {
+            this.popper.popper.setAttribute('x-loading-error', '');
+          },
+          complete: (data) => {
+            this.popper.popper.removeAttribute('x-loading');
+            resolve();
+          },
+        });
+      } else {
+        resolve();
+      }
+    }).then(() => {
+      this.updateTooltipContent(this.triggerElem, this.type);
+    });
+  };
 
   // Watch for changes to title, data-title, data-tooltip, and update the tooltip contents accordingly.
   AhaTooltip.prototype.titleContentMutationCallback = function(tooltip, type, mutations, observer) {
     mutations.forEach((mutation) => {
-      const newValue = mutation.target.getAttribute(mutation.attributeName);
+      let newValue = mutation.target.getAttribute(mutation.attributeName);
+      let value;
+      try {
+        newValue = JSON.parse(newValue);
+        value = typeof newValue === 'object' ? newValue[type] : newValue;
+      } catch (e) {
+        value = newValue;
+      }
+
       if (mutation.attributeName === 'data-content') {
-        tooltip[type].tooltip.opts.content = newValue;
+        tooltip[type].tooltip.opts.content = value;
       } else {
-        tooltip[type].tooltip.opts.title = newValue;
+        tooltip[type].tooltip.opts.title = value;
         if (mutation.attributeName === 'title') {
-          mutation.target.dataset.title = newValue;
+          mutation.target.dataset.title = value;
           mutation.target.removeAttribute('title');
         }
       }
       this.updateTooltipContent(mutation.target, type);
       observer.takeRecords();
     });
-  }
+  };
+
+  // Watch for triggerElem removals, when that happens destroy the tooltip and remove the observer.
+  AhaTooltip.prototype.triggerElemMutationCallback = function(triggerElem, mutations, observer) {
+    mutations.forEach((mutation) => {
+      if (Array.from(mutation.removedNodes).includes(triggerElem)) {
+        this.destroyTooltip();
+        observer.disconnect();
+      }
+    });
+  };
 
   AhaTooltip.prototype.updateTooltipContent = function() {
-    const {title, content} = this.opts;
+    const { title, content } = this.opts;
     const titleElem = this.popper.popper.querySelector('[x-title]');
     const contentElem = this.popper.popper.querySelector('[x-content]');
 
     if (this.opts.html) {
-      titleElem.innerHTML = this.cleanHTML(title) || '';
-      contentElem.innerHTML = this.cleanHTML(content) || '';
+      if (this.opts.allowUnsafe) {
+        // Pass any HTML through without modification.
+        titleElem.innerHTML = title || '';
+        contentElem.innerHTML = content || '';
+      } else {
+        // Remove any suspect tags/attributes/attribute values.
+        titleElem.innerHTML = this.cleanHTML(title) || '';
+        contentElem.innerHTML = this.cleanHTML(content) || '';
+      }
     } else {
+      // Render as plain text, without parsing any HTML.
       titleElem.textContent = title || '';
       contentElem.textContent = content || '';
     }
     this.popper.update();
-  }
+  };
 
   // Escaping dangerous HTML content **SHOULD** be done server-side, however, people occasionally forget to do this.
   // `cleanHTML` serves as a measure of last resort, removing explicitly dangerous tags, removing any non-whitelisted attributes,
@@ -209,13 +260,13 @@
       }
     }
     return body.innerHTML;
-  }
+  };
 
 
 
   AhaTooltip.prototype.repositionTooltip = function() {
     this.popper.update();
-  }
+  };
 
   AhaTooltip.prototype.closeTooltip = function(delayHide) {
     const self = this;
@@ -225,35 +276,35 @@
         self.triggerElem.removeAttribute('x-tooltip');
         self.isVisible = false;
         self.timeout = setTimeout(() => {
-          self.popper.popper.addEventListener('transitionend', function fadeOut() {
-            self.popper.popper.removeEventListener('transitionend', fadeOut);
-            if (self.popper.popper.parentNode) {
-              self.popper.popper.parentNode.removeChild(self.popper.popper);
-            }
-          });
+          if (!self.isVisible) {
+            self.popper.popper.addEventListener('transitionend', function fadeOut() {
+              self.popper.popper.removeEventListener('transitionend', fadeOut);
+              if (self.popper.popper.parentNode && !self.isVisible) {
+                self.popper.popper.parentNode.removeChild(self.popper.popper);
+              }
+            });
+          }
         }, typeof delayHide === 'undefined' ? self.opts.delay.hide : delayHide);
         self.popper.popper.removeAttribute('x-in');
       }
     }
-  }
+  };
 
   AhaTooltip.prototype.enableTooltip = function() {
     this.enabled = true;
-  }
+  };
 
   AhaTooltip.prototype.disableTooltip = function() {
     this.closeTooltip(0);
     this.enabled = false;
-  }
+  };
 
   AhaTooltip.prototype.destroyTooltip = function() {
     this.closeTooltip(0);
     const data = AhaTooltip.prototype.getData(this.triggerElem);
     delete data[this.type];
-  }
+  };
 
-  // ---------- METHODS SPECIFIC TO THE tooltip/popover plugins ----------
-  // Cannot be class methods as they are called on elements which do not yet have an associated TOOLTIP class.
   AhaTooltip.prototype.getOnOffEvents = function(opts) {
     return AhaTooltip.prototype.triggers(opts).reduce((acc, trigger) => {
       if (trigger === 'click') {
@@ -265,15 +316,17 @@
       }
       return acc;
     }, []);
-  }
+  };
 
   AhaTooltip.prototype.triggers = function(opts) {
     return opts.trigger.split(' ').map(x => x.trim());
-  }
+  };
 
   AhaTooltip.prototype.mergeInlineOpts = function(elem, opts = {}) {
     // Move legacy data-tooltip, title attributes to data-title.
-    if (elem.dataset.tooltip) {
+    // debugger
+    // Handle bootstrap legacy way of sometimes setting a title for a tooltip.
+    if (elem.dataset.tooltip && typeof JSON.parse(elem.dataset.tooltip) !== 'boolean') {
       elem.dataset.title = elem.dataset.tooltip;
       delete elem.dataset.tooltip;
     }
@@ -288,35 +341,37 @@
       let value = elem.dataset[key];
       try {
         value = JSON.parse(value.replace(/'/g, '"'));
-        if (Array.isArray(value) || key === 'delay') {
+        if (Array.isArray(value) || key === 'delay' || typeof value === 'boolean') {
           opts[key] = value;
         } else {
           if (typeof value[opts.type] !== 'undefined') {
             opts[key] = value[opts.type];
           }
         }
-      } catch {
+      } catch(e) {
         opts[key] = value;
       }
     });
     return opts;
-  }
+  };
 
   AhaTooltip.prototype.getData = function(elem) {
-    const $elem = elem.nodeType ? $(elem) : elem;
-    let data = $.data(elem)[namespace]  || {};
-    return data
-  }
+    return $.data(elem)[namespace] || {};
+  };
+
+  AhaTooltip.prototype.setData = function(data) {
+    $.data(this.triggerElem)[namespace] = data;
+  };
 
   AhaTooltip.prototype.getTooltip = function(triggerElem, type) {
     const data = AhaTooltip.prototype.getData(triggerElem);
     return data[type] && data[type].tooltip;
-  }
+  };
 
   AhaTooltip.prototype.getOrCreateTooltip = function(triggerElem, opts) {
     opts = AhaTooltip.prototype.mergeInlineOpts(triggerElem, opts);
     return AhaTooltip.prototype.getTooltip(triggerElem, opts.type) || new AhaTooltip(triggerElem, opts);
-  }
+  };
 
   AhaTooltip.prototype.bindEvents = function(context, selector, opts) {
     const events = AhaTooltip.prototype.getOnOffEvents(opts);
@@ -325,59 +380,68 @@
       if (offEvent) {
         $(context).on(onEvent, selector, (e) => {
           const tooltip = AhaTooltip.prototype.getOrCreateTooltip(e.currentTarget, opts);
-          tooltip.openTooltip();
+          if (!tooltip.data.preventEventExcept || tooltip.data.preventEventExcept === onEvent) {
+            tooltip.openTooltip();
+          }
         }).on(offEvent, selector, (e) => {
           const tooltip = AhaTooltip.prototype.getOrCreateTooltip(e.currentTarget, opts);
-          const triggerElem = e.currentTarget
+          if (!tooltip.data.preventEventExcept || tooltip.data.preventEventExcept === offEvent) {
+            const triggerElem = e.currentTarget;
 
-          if (triggerElem.contains(e.relatedTarget)) return;
-          if (offEvent === 'mouseout' && e.relatedTarget === tooltip.popper.popper) {
-            $(tooltip.popper.popper).on('mouseout', function mouseLeaveHandler(mouseoutEvent) {
-              if (!tooltip.popper.popper.contains(mouseoutEvent.relatedTarget) && !triggerElem.contains(mouseoutEvent.relatedTarget)) {
-                tooltip.closeTooltip();
-                $(tooltip.popper.popper).off('mouseleave', mouseLeaveHandler);
-              }
-            });
-          } else {
-            tooltip.closeTooltip();
+            if (triggerElem.contains(e.relatedTarget)) return;
+            if (offEvent === 'mouseout' && e.relatedTarget === tooltip.popper.popper) {
+              $(tooltip.popper.popper).on('mouseout', function mouseLeaveHandler(mouseoutEvent) {
+                if (!tooltip.popper.popper.contains(mouseoutEvent.relatedTarget) && !triggerElem.contains(mouseoutEvent.relatedTarget)) {
+                  tooltip.closeTooltip();
+                  $(tooltip.popper.popper).off('mouseleave', mouseLeaveHandler);
+                }
+              });
+            } else {
+              tooltip.closeTooltip();
+            }
           }
         });
       } else {
         $(context).on(onEvent, selector, (e) => {
           const triggerElem = e.currentTarget;
           const tooltip = AhaTooltip.prototype.getOrCreateTooltip(triggerElem, opts);
-          let handleClickOutside;
-          if (tooltip.isVisible) {
-            tooltip.closeTooltip();
-          } else {
-            tooltip.openTooltip();
-            requestAnimationFrame(() => {
-              $(document).on(onEvent, function handleClickOutside(clickOutsideEvent) {
-                if (triggerElem === clickOutsideEvent.target) {
-                  $(document).off(onEvent, handleClickOutside);
-                } else {
-                  if (!tooltip.popper.popper.contains(clickOutsideEvent.target)) {
+          if (!tooltip.data.preventEventExcept || tooltip.data.preventEventExcept === onEvent) {
+            if (tooltip.isVisible) {
+              tooltip.closeTooltip();
+              tooltip.clearPreventEventExcept();
+            } else {
+              tooltip.openTooltip();
+              tooltip.setPreventEventExcept(onEvent);
+              requestAnimationFrame(() => {
+                $(document).on(onEvent, function handleClickOutside(clickOutsideEvent) {
+                  const selectroModal = document.querySelector('#selectro-modal');
+                  if (triggerElem === clickOutsideEvent.target) {
                     $(document).off(onEvent, handleClickOutside);
-                    tooltip.closeTooltip();
+                  } else {
+                    if (!tooltip.popper.popper.contains(clickOutsideEvent.target) && (!selectroModal || !selectroModal.contains(clickOutsideEvent.target))) {
+                      $(document).off(onEvent, handleClickOutside);
+                      tooltip.closeTooltip();
+                      tooltip.clearPreventEventExcept();
+                    }
                   }
-                }
-              })
-            })
+                });
+              });
+            }
           }
         });
       }
     });
-  }
+  };
 
   AhaTooltip.prototype.createManualTooltips = function(context, selector, opts) {
-    $(`${context} ${selector ? selector : ''}`).each((i, elem) => {
+    $(`${context} ${selector || ''}`).each((i, elem) => {
       AhaTooltip.prototype.getOrCreateTooltip(elem, opts);
     });
-  }
+  };
 
   AhaTooltip.prototype.manualTypeMutationCallback = function(context, selector, opts, mutations) {
-    AhaTooltip.prototype.createManualTooltips(context, selector, opts)
-  }
+    AhaTooltip.prototype.createManualTooltips(context, selector, opts);
+  };
 
   AhaTooltip.prototype.setup = function(opts, type) {
     // Set context and selector for event assignment.
@@ -434,17 +498,16 @@
           });
           break;
         case 'dispose':
+        case 'destroy':
           this.each((i, elem) => {
             const tooltip = AhaTooltip.prototype.getTooltip(elem, type);
             if (tooltip) tooltip.destroyTooltip();
           });
           break;
         default:
-          return;
       }
     }
-  }
-  //-----------------
+  };
 
   $.fn.tooltip = function(opts = {}) {
     AhaTooltip.prototype.setup.call(this, opts, 'tooltip');
